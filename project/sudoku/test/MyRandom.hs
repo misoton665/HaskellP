f28e37799398a13569c8ce11af41e2ddf09c52e3
misoton665 <misoton998@hotmail.co.jp>
2015-09-17 04:35:42 +0900

    Move sudoku to old_sudoku

--- project/sudoku/test/MyRandom.hs
+++
@@ -1,9 +0,0 @@
-module MyRandom(rand, rand3) where
-
-import System.Random
-
-rand :: (Num a, Random a) => (a, a) -> IO a
-rand r = getStdRandom $ randomR r
-
-rand3 :: IO Integer
-rand3 = rand (0, 2)
