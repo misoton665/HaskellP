68f08c76fcabcc1de2f1854f7d564d282d0ac691
misoton665 <misoton998@hotmail.co.jp>
2015-09-24 04:00:42 +0900

    Create Sudoku/Editor.hs

---
+++ project/sudoku/Sudoku/Editor.hs
@@ -0,0 +1,13 @@
+module Sudoku.Editor (
+  editStage,
+  editSquare
+  ) where
+
+import Sudoku.Data
+import Sudoku.Generator
+
+editStage :: SDPosition -> SDNum -> SDStage -> SDStage
+editStage = undefined
+
+editSquare :: SDNum -> SDSquare
+editSquare = undefined
